1692718f01c7c19bd81664ee5fef3d3841bf1a81
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-24 00:29:33 +0800

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,40 @@
+
+public class ForLoop 
+{
+	public static void main(String[] args) 
+	{
+		double[] arr1 = new double[5];
+		//int[] storing multiple integer values
+		//arr variable name
+		//new creates new memory for multiple values that will be stores
+		
+		arr1[0] = 10; //declaring values on each array
+		arr1[1] = 20;
+		arr1[2] = 30;
+		arr1[3] = 40;
+		arr1[4] = 50.5;
+		
+		int[] arr2 = {4, 8, 12, 16, 20, 24, 28, 32,36, 40}; //another way of storing values in an array
+		System.out.println(arr2[0]);
+		System.out.println(arr2[2]);
+		
+		//For loop with array length equals to 5
+		for (int i = 0; i < arr1.length; i++) 
+		{
+			System.out.println(arr1[i]);
+		}
+		
+		//For loop with array length equals to 10
+		for (int i = 0; i < arr2.length; i++) 
+		{
+			System.out.println(arr2[i]);
+		}
+		
+		String[] names = {"Spongebob", "Patrick", "Squidward", "Mr. Krabs"};
+		
+		for (int i = 0; i < names.length; i++) 
+		{
+			System.out.println(names[i]);
+		}
+	}
+}
