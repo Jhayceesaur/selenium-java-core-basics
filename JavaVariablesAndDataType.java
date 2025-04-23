486137f9f62543570dc7e112125e92fe43e3d499
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-23 23:26:05 +0800

    Add files via upload

---
+++ JavaVariablesAndDataType.java
@@ -0,0 +1,20 @@
+
+public class JavaVariablesAndDataType 
+{
+	public static void main(String[] args) 
+	{
+		int number = 04; //Data type is integer "int"; Variable is "number"; Value is "5"
+		String website = "Udemy Online Courses";
+		char letter = 'r';
+		double decimal = 20.17; //Can use also float data type
+		boolean myCard = true;
+		
+		System.out.println(number);
+		System.out.println(website);
+		System.out.println(letter);
+		System.out.println(decimal);
+		System.out.println(myCard);
+		
+		System.out.println("Please visit " + website + ". Thank you!"); //Concatenate
+	}
+}
