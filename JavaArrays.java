0a213e22d429c7bd57ae264a80a168c01c7b1e7e
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-23 23:36:52 +0800

    Add files via upload

---
+++ JavaArrays.java
@@ -0,0 +1,21 @@
+
+public class JavaArrays 
+{
+	public static void main(String[] args) 
+	{
+		double[] arr1 = new double[5];
+		//int[] storing multiple integer values
+		//arr variable name
+		//new creates new memory for multiple values that will be stores
+		
+		arr1[0] = 10; //declaring values on each array
+		arr1[1] = 20;
+		arr1[2] = 30;
+		arr1[3] = 40;
+		arr1[4] = 50.5;
+		
+		int[] arr2 = {4, 8, 12, 16, 20}; //another way of storing values in an array
+		System.out.println(arr2[0]);
+		System.out.println(arr2[2]);
+	}
+}
