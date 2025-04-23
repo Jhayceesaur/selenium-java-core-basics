a4fc886e76de1771d7d7f99d59b9c4bb1bddd4d3
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-24 01:06:13 +0800

    Add files via upload

---
+++ DynamicArray.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class DynamicArray 
+{
+	public static void main(String[] args) 
+	{
+		//Array List concept
+		ArrayList<String> a = new ArrayList<String>(); //Class / Object name = new / Class name
+		a.add("Gengar");
+		a.add("Scyther");
+		a.add("Pikachu");
+		a.add("Lapras");
+		a.add("Snorlax");
+		a.add("Alakazam");
+		a.add("Voltorb");
+		System.out.println(a.remove(6));
+		System.out.println(a.get(0));
+
+	}
+}
